32bcd544f15cfb9b4d0e6e35735d8887e4f60204
KilonzoJames <jay.kilonzo@gmail.com>
2023-06-11 16:22:01 +0300

    dom-modifying-elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainn= document.getElementById("#main");
+mainn.remove();
+
+const element=document.createElement("h1");
+document.body.append(element);
+element.textContent="James is the champion";
+
+element.id="victory";
+
+main.innerHTML ="<h1>James is the champion</h1>";
